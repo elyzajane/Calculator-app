d1b3c448432b90afcfd69a4ebe254cb3f0c2e947
Elyza <elyzajanerelucio@gmail.com>
2023-05-12 20:45:19 +0800

    Add colors to make the code creative

--- Calculator_app.py
+++ Calculator_app.py
@@ -4,7 +4,7 @@
 # Display a greeting message for the user
 print("\033[1;35;40m╦ ╦ ╦═╗╦  ╦  ╔═╗")
 print("\033[1;35;40m╠═╣ ╠═ ║  ║  ║ ║")
-print("\033[1;35;40m╩ ╩ ╩═╝╩═╝╩═╝╚═╝\n")
+print("\033[1;35;40m╩ ╩ ╩═╝╩═╝╩═╝╚═╝")
 
 # Display a message reminding the user to use the calculator app wisely
 print("\033[1;31;40mKindly use the calculator app wisely!\033[0m")
@@ -23,15 +23,15 @@ def operate(num1, num2, op):
     elif op == "/":
         return operator.truediv(num1, num2)
     else:
-        raise ValueError("Invalid operator")
+        raise ValueError("\033[1;31;40mInvalid operator\033[0m")
 # Use def function to get user input 
 def calculate():
 # Display the options
-    print("Please choose the operation.")
-    print("Addition")
-    print("Subtraction")
-    print("Multiplication")
-    print("Division")
+    print("\033[1;33;40mPlease choose the operation.\033[0m")
+    print("\033[1;32;40m1. Addition\033[0m")
+    print("\033[1;36;40m2. Subtraction\033[0m")
+    print("\033[1;34;40m3. Multiplication\033[0m")
+    print("\033[1;35;40m4. Division\033[0m")
 
 # Get user input for the chosen operation
     while True:
@@ -42,7 +42,7 @@ def calculate():
                 raise ValueError
             break
         except ValueError:
-            print("Invalid choice! Please enter a valid choice.")
+            print("A! Please enter a valid choice.")
 # Ask the user to input two numbers to perform the operation
     while True:
         try:
