b4dfa7d8371a0e12ead521c25d8105c07f69d853
Elyza <elyzajanerelucio@gmail.com>
2023-05-12 16:19:21 +0800

    Program Outline

---
+++ Calculator_app.py
@@ -0,0 +1,10 @@
+# Use def function to perform mathematical operations
+# Use if-else statements to know which operation is needed
+# Use def function to get user input 
+# Display the options
+# Get user input for the chosen operation
+# An error  message will apper if the user enters an invalid choice
+# Ask the user to input two numbers to perform the operation
+# An error message will appear if the user enter invalid number then ask to enter their choice again
+# Perform the selected operation
+# Print the result
